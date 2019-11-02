2fb55b581844e1aec1c3b09b4e7a3293a7a2dc55
raytheguy <55873005+raytheguy@users.noreply.github.com>
2019-11-02 16:17:14 +1100

    Created basic layout
    Created basic layout

--- StudyMore/app/src/main/java/com/example/studymore/MainActivity.java
+++ StudyMore/app/src/main/java/com/example/studymore/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.studymore;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    public ImageButton settingsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        settingsButton = findViewById(R.id.settingsButton);
+        settingsButton.setImageResource(R.drawable.settingsclog);
     }
+
+
 }
