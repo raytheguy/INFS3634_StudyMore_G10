8c66485daab3f7b31a4d6429f6cd2a8e46115f82
raytheguy <55873005+raytheguy@users.noreply.github.com>
2019-11-13 23:39:56 +1100

    Changed Main Page Layout
    Changed Main Page Layout

--- StudyMore/app/src/main/java/com/example/studymore/MainActivity.java
+++ StudyMore/app/src/main/java/com/example/studymore/MainActivity.java
@@ -1,20 +1,44 @@
 package com.example.studymore;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     public ImageButton settingsButton;
 
+    //create all the
+    //recyclerview unsuitable for this situation
+    //everything is static
+    public TextView text1;
+    public ConstraintLayout box1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        settingsButton = findViewById(R.id.settingsButton);
+        settingsButton = findViewById(R.id.settingsClog);
         settingsButton.setImageResource(R.drawable.settingsclog);
+
+        //set Box 1
+        box1 = findViewById(R.id.featureone);
+        text1 = box1.findViewById(R.id.featureName);
+        text1.setText("Encyclopedia");
+
+        //set Box 2
+
+        //set Box 3
+
+        //set Box 4
+
+        //set Box 5
+
+        //set Box 6
+
     }
 
 
